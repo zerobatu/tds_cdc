24e5de2ab09956c4f7ec2037fac37368278cb457
Claudio Alvarado <claudio@modelab.cl>
2026-05-25 16:15:33 -0400

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdsCdc.MixProject do
   def project do
     [
       app: :tds_cdc,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
